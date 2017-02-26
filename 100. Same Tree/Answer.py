6a6303191b280b4e058b71f71afa2c4ae5e8bc3e
nagao <niko_fan@sina.com>
2017-02-26 20:16:56 +0800

    Leetcode Question 100 Python.

---
+++ 100. Same Tree/Answer.py
@@ -0,0 +1,25 @@
+#解题思路：
+#1.递归问题，遍历
+#2.首先特殊情况：两者均为空
+#3.普遍情况：抽象成一个点，判断值是否相同？左支点相同？右支点相同？
+#4.重复步骤2
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p==None and q==None:
+            return True
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right,q.right)
+        return False
