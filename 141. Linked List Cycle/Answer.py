2662de803f3dbc66b44a4d74659381860f560c5c
nikoGao <niko_fan@sina.com>
2017-03-01 17:34:48 +0800

    Leetcode Questiion 141 Python

---
+++ 141. Linked List Cycle/Answer.py
@@ -0,0 +1,22 @@
+#快慢指针，slow走1步，fast走2步。如果链表不存在环，则fast或者fast.next先走到None。若存在环，则slow和fast总会碰到
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head == None or head.next == None:
+            return False
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
