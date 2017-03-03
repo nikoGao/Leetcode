403eaf6d4b3ec84d19f02dbfd7d12365f657c3ee
nagao <nagao@nagao.redhat.com>
2017-03-03 10:16:18 +0800

    Leetcode Question 169 Python

---
+++ 169. Majority Element/Answer.py
@@ -0,0 +1,17 @@
+class Solution(object):
+
+    def majorityElement(self, nums):
+
+        """
+
+        :type nums: List[int]
+
+        :rtype: int
+
+        """
+
+        nums.sort()
+
+        mid = len(nums)/2
+
+        return nums[mid]
