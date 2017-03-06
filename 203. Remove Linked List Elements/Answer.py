221e3dd620575d78685256b30d1a7ca8a4f2fdc8
nikoGao <niko_fan@sina.com>
2017-03-06 11:20:20 +0800

    Leetcode Question 203 Python

---
+++ 203. Remove Linked List Elements/Answer.py
@@ -0,0 +1,24 @@
+#使用哑节点，导入一个头，用循环遍历，用两个指针记录当前和前一个节点
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        pre, cur = dummy, head
+        while cur:
+            if cur.val == val:
+                pre.next = cur.next
+            else:
+                pre = cur
+            cur = cur.next
+        return dummy.next
