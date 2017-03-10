482d16c36e913293f65b8ae3d67a6ef0f08fd220
nagao <nagao@nagao.redhat.com>
2017-03-10 10:51:41 +0800

    Leetcode Question 263 Python

---
+++ 263. Ugly Number/Answer.py
@@ -0,0 +1,22 @@
+#将输入的数重复除2，3，5，判断最后是否为1
+class Solution(object):
+
+    def isUgly(self, num):
+
+        """
+
+        :type num: int
+
+        :rtype: bool
+
+        """
+
+        if num < 0: return False
+
+        for i in [2,3,5]:
+
+            while num % i == 0:
+
+                num /= i
+
+        return num == 1
