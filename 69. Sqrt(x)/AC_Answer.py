7522552d33c8dee9f236338a401fa5e223b96b46
nikoGao <niko_fan@sina.com>
2017-04-02 11:51:32 +0800

    Improvment, but use math.sqrt, actually, not as good as previous

---
+++ 69. Sqrt(x)/AC_Answer.py
@@ -0,0 +1,9 @@
+# Time: 46 ms(93%)
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        res = int(math.sqrt(x))
+        return res
