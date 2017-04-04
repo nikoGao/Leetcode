0080b4412268b8d8a5f61f1269a2445df89c4cf7
nikoGao <niko_fan@sina.com>
2017-04-04 16:24:58 +0800

    Use DFS+STACK

---
+++ 107. Binary Tree Order Traversal II/AC_Answer.py
@@ -0,0 +1,27 @@
+# Run time 56ms
+# DFS + Stack, basically, the main code is same as dfs
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        stack = [(root, 0)]
+        res = []
+        while stack:
+            node, level = stack.pop()
+            if node:
+                if len(res) < level+1:
+                    res.insert(0, [])
+                res[-(level+1)].append(node.val)
+                stack.append((node.right, level+1))
+                stack.append((node.left, level+1))
+        return res
