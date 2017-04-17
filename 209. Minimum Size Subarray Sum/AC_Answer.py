b178e7c58897edf85e9403ffd05196c4a93fed0f
nagao <nagao@redhat.com>
2017-04-17 14:45:01 +0800

    Leetcode Question 209, use moving window, O(n)

---
+++ 209. Minimum Size Subarray Sum/AC_Answer.py
@@ -0,0 +1,37 @@
+# Move the window, use two point, add/delete num in the window
+
+class Solution(object):
+
+    def minSubArrayLen(self, s, nums):
+
+        """
+
+        :type s: int
+
+        :type nums: List[int]
+
+        :rtype: int
+
+        """
+
+        total = left = 0
+
+        result = len(nums) + 1
+
+        for right, n in enumerate(nums):
+
+            total += n
+
+            while total >= s:
+
+                result = min(result, right-left+1)
+
+                total -= nums[left]
+
+                left += 1
+
+        return result if result <=len(nums) else 0
+
+        
+
+        
