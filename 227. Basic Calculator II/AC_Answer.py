49a57204fcfe5ac48191237114d19101537b2778
nagao <nagao@redhat.com>
2017-04-19 11:16:06 +0800

    Leetcode Question 227, important

---
+++ 227. Basic Calculator II/AC_Answer.py
@@ -0,0 +1,59 @@
+# Use stack to store nums beside '+-', use negative num to present as '-'
+# Use a temp to store previous operator, and when meet next operator, make a decision and caculate
+# Pay attention to the num, whether it is ten or hundreds
+
+class Solution(object):
+
+    def calculate(self, s):
+
+        """
+
+        :type s: str
+
+        :rtype: int
+
+        """
+
+        if not s:
+
+            return "0"
+
+        stack, num, sign = [], 0, "+"
+
+        for i in xrange(len(s)):
+
+            if s[i].isdigit():
+
+                num = num*10+ord(s[i])-ord("0")
+
+            if (not s[i].isdigit() and not s[i].isspace()) or i == len(s)-1:
+
+                if sign == "-":
+
+                    stack.append(-num)
+
+                elif sign == "+":
+
+                    stack.append(num)
+
+                elif sign == "*":
+
+                    stack.append(stack.pop()*num)
+
+                else:
+
+                    tmp = stack.pop()
+
+                    if tmp//num < 0 and tmp%num != 0:
+
+                        stack.append(tmp//num+1)
+
+                    else:
+
+                        stack.append(tmp//num)
+
+                sign = s[i]
+
+                num = 0
+
+        return sum(stack)
