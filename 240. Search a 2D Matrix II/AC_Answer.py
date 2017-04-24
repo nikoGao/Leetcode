8cc6050aaf8401ad32400193a0e5ac7c2208be09
nagao <nagao@redhat.com>
2017-04-24 11:13:43 +0800

    Leetcode Question 240 Python

---
+++ 240. Search a 2D Matrix II/AC_Answer.py
@@ -0,0 +1,35 @@
+ass Solution(object):
+
+    def searchMatrix(self, matrix, target):
+
+        """
+
+        :type matrix: List[List[int]]
+
+        :type target: int
+
+        :rtype: bool
+
+        """
+
+        if matrix == [] or matrix == [[]]: return False
+
+        m, n = len(matrix), len(matrix[0])
+
+        r, c = 0, n-1
+
+        while r<m and c>=0:
+
+            if matrix[r][c] == target:
+
+                return True
+
+            if matrix[r][c] > target:
+
+                c -= 1
+
+            else:
+
+                r += 1
+
+        return False
