3f5d6cd30ecfe19ab15c3c92bfc30977e5ea32ed
nikoGao <niko_fan@sina.com>
2017-05-06 11:17:06 +0800

    Leetcode Question 289, normal thinking

---
+++ 289. Game of Life/AC_Answer.py
@@ -0,0 +1,25 @@
+# There are four situations: 00, 01, 10, 11, then in int:0,1,2,3
+class Solution(object):
+    def gameOfLife(self, board):
+        """
+        :type board: List[List[int]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        m = len(board)
+        n = len(board[0])
+        if m == 0 or n == 0:
+            return 
+        for iM in range(m):
+            for iN in range(n):
+                numNeighbor = sum([board[i][j]%2 for i in range(iM-1,iM+2) for j in range(iN-1,iN+2) if 0 <= i < m and 0<= j < n]) - board[iM][iN]
+                if board[iM][iN] == 0 and numNeighbor == 3:
+                    board[iM][iN] = 2
+                if board[iM][iN] == 1 and ( numNeighbor < 2 or numNeighbor >  3):
+                    board[iM][iN] = 3
+        for iM in range(m):
+            for iN in range(n):
+                if board[iM][iN] == 2:
+                    board[iM][iN] = 1
+                if board[iM][iN] == 3:
+                    board[iM][iN] = 0
+                    
