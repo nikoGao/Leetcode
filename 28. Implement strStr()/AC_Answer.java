d0366530f794fdcd3e848d5921130196b339897b
Niko Gao <niko_fan@sina.com>
2017-09-12 13:11:33 -0400

    Java Solution with one line, runtime is 6ms

---
+++ 28. Implement strStr()/AC_Answer.java
@@ -0,0 +1,7 @@
+// Use String.indexOf() to return the first apperance of substring
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
