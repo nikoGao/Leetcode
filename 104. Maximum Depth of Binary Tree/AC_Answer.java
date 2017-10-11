428d84e7f34cfcf300cd3f1e86df64cd5a4f5021
Niko Gao <niko_fan@sina.com>
2017-10-11 18:02:44 -0400

    Use dfs easy java code

---
+++ 104. Maximum Depth of Binary Tree/AC_Answer.java
@@ -0,0 +1,18 @@
+//dfs
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.lang.Math;
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root != null) return 1+ Math.max(maxDepth(root.left), maxDepth(root.right));
+        else return 0;
+    }
+}
