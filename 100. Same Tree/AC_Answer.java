07a826560c63263767bc3af2a82742866284e0a2
Niko Gao <niko_fan@sina.com>
2017-10-11 10:54:47 -0400

    Java solution, recursion tree, notify node==null

---
+++ 100. Same Tree/AC_Answer.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        else if(p!=null && q!=null){
+            if(p.val == q.val){
+                return (isSameTree(p.left, q.left)==true && isSameTree(p.right, q.right)==true);
+            }else {
+                return false;
+            }
+        }else{
+            return false;
+    }
+    }
+}
