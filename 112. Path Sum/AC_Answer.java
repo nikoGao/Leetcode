0d41be25952f0e980af9317088e59919fa0beaac
Niko Gao <niko_fan@sina.com>
2017-10-14 16:59:39 -0400

    Use easy recursion

---
+++ 112. Path Sum/AC_Answer.java
@@ -0,0 +1,19 @@
+//Should separate root==null and root!=null && root.left==null && root.right==null
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root==null) return false;
+        sum -= root.val;
+        if(root.left==null&&root.right==null) return sum==0;
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+}
