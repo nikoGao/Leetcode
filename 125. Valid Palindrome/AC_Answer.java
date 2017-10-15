e1f35c7b4f8927bf1eef1abd48ee1e88ecb161fe
Niko Gao <niko_fan@sina.com>
2017-10-15 15:17:41 -0400

    Classic problem, can do both in Java, C++ and Python

---
+++ 125. Valid Palindrome/AC_Answer.java
@@ -0,0 +1,16 @@
+// classic problem, O(n) time
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0 ,j = s.length()-1;
+        while(i<j){
+            if(!Character.isLetterOrDigit(s.charAt(i))) i++;
+            else if(!Character.isLetterOrDigit(s.charAt(j))) j--;
+            else if(Character.toLowerCase(s.charAt(i))==Character.toLowerCase(s.charAt(j))){
+                i++;
+                j--;
+            }else return false;
+        }
+        return true;
+    }
+}
