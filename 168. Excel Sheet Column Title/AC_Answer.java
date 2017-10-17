5ec5850c08dc4571f596fecfd43279167dc82eb1
Niko Gao <niko_fan@sina.com>
2017-10-16 20:44:46 -0400

    Easy recursion problem

---
+++ 168. Excel Sheet Column Title/AC_Answer.java
@@ -0,0 +1,7 @@
+// Use recursion, each time add one elem on the end of String
+
+class Solution {
+    public String convertToTitle(int n) {
+        return n == 0 ? "" : convertToTitle(--n / 26) + (char)('A' + (n % 26));
+    }
+}
