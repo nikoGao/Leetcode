9ff8bee2bb98da8c4e313a4378ee83a1d45f4d54
Niko Gao <niko_fan@sina.com>
2017-10-18 12:06:21 -0400

    How to treat it as unsigned number

---
+++ 190. Reverse Bits/AC_Answer.java
@@ -0,0 +1,16 @@
+// bit manipulate, result<<=1, number>>>=1, notice treat it as unsigned
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        for(int i = 0; i< 32; i++){
+            result += n & 1;
+            n >>>=1;
+            if(i<31){
+                result <<=1;
+            }
+        }
+        return result;
+    }
+}
