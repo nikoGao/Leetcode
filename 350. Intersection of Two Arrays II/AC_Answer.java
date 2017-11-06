ecd70d0216935ab7262b7476017ce1b6cf278c80
Niko Gao <niko_fan@sina.com>
2017-11-06 18:07:13 -0500

    with duplicate with 349

---
+++ 350. Intersection of Two Arrays II/AC_Answer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> list = new ArrayList<Integer>();
+        int i = 0, j= 0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]==nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i] < nums2[j]) i++;
+            else if(nums1[i] > nums2[j]) j++;
+        }
+        int[] arr = new int[list.size()];
+        int k = 0;
+        for(Integer num:list){
+            arr[k++] = num;
+        }
+        return arr;
+    }
+}
