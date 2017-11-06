59a35a38d66f5ce76ad8d7e2c279d67816ad3107
Niko Gao <niko_fan@sina.com>
2017-11-06 17:48:30 -0500

    Keep tracking sum

---
+++ 346. Moving Average from Data Stream/AC_Answer.java
@@ -0,0 +1,20 @@
+public class MovingAverage{
+	private int [] window;
+	private int n, insert;
+	private long sum;
+	public MovingAverage(int size){
+		window = new int[size];
+		insert = 0;
+		n = 0;
+	}
+
+	public double next(int val){
+		if (n < window.length)  n++;
+        sum -= window[insert];
+        sum += val;
+        window[insert] = val;
+        insert = (insert + 1) % window.length;
+        
+        return (double)sum / n;
+	}
+}
