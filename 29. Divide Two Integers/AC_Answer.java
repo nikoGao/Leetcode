6257622bca13a992931c4c44263e6664c9f395e4
Niko Gao <niko_fan@sina.com>
2018-01-22 16:29:53 -0500

    Use substract, use long to avoid the overflow

---
+++ 29. Divide Two Integers/AC_Answer.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        int sign = 1;
+        if((dividend>0 && divisor<0) || (dividend<0 && divisor>0)) sign = -1;
+        long ldividend = Math.abs((long)dividend);
+        long ldivisor = Math.abs((long)divisor);
+        if(ldivisor==0) return Integer.MAX_VALUE;
+        if(ldividend==0||(ldividend<ldivisor)) return 0;
+        
+        long lans = ldivide(ldividend, ldivisor);
+        int ans;
+        if(lans>Integer.MAX_VALUE) ans = (sign==1)?Integer.MAX_VALUE:Integer.MIN_VALUE;
+        else ans = (int)(sign * lans);
+        return ans;
+    }
+    
+    public long ldivide(long ldividend, long ldivisor){
+        if(ldividend<ldivisor) return 0;
+        long sum = ldivisor;
+        long times = 1;
+        while((sum+sum)<=ldividend){
+            sum += sum;
+            times += times;
+        }
+        return times + ldivide(ldividend-sum, ldivisor);
+    }
+}
