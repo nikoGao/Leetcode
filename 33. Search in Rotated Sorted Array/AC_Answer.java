55b8f7e39b36a6e7f47b1b0a38bb33d196d4e804
Niko Gao <niko_fan@sina.com>
2018-01-23 10:39:23 -0500

    Split into four circumstances, compare target each with left&mid or right&mid

---
+++ 33. Search in Rotated Sorted Array/AC_Answer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length==0) return -1;
+        int left = 0, right = nums.length-1;
+        while(left<right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[left]<=nums[mid]){
+                if(target>=nums[left] && target < nums[mid]) right = mid-1;
+                else left = mid+1;
+            }else{
+                if(target > nums[mid] && target<=nums[right]) left = mid+1;
+                else right = mid-1;
+            }
+        }
+        return nums[left]==target?left:-1;
+    }
+}
