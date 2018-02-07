4f96702f24d1b14ff603f4c8498e46a90ad58c27
Niko Gao <niko_fan@sina.com>
2018-02-06 19:28:38 -0500

    Depth first searching, use map to track already built node

---
+++ 133. Clone Graph/AC_Answer.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    Map<Integer, UndirectedGraphNode> map = new HashMap<>();
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        return clone(node);
+    }
+    
+    public UndirectedGraphNode clone(UndirectedGraphNode node){
+        if(node==null) return null;
+        if(map.containsKey(node.label)) return map.get(node.label);
+        UndirectedGraphNode clone = new UndirectedGraphNode(node.label);
+        map.put(clone.label, clone);
+        for(UndirectedGraphNode n: node.neighbors){
+            clone.neighbors.add(clone(n));
+        }
+        return clone;
+    }
+}
