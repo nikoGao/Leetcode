2d68c409a28742976e220c2df375b57fe3c528e9
Niko Gao <niko_fan@sina.com>
2018-03-02 17:02:23 -0500

    The end of the test point

---
+++ 228. Summary Ranges/AC_Answer.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        if(nums.length==0) return res;
+        if(nums.length==1){
+            res.add(nums[0]+"");
+            return res;
+        }
+        for(int i=0;i<nums.length;i++){
+            int a=nums[i];
+            while(i+1<nums.length&&(nums[i+1]-nums[i])==1){
+                i++;
+            }
+            if(a!=nums[i]){
+                res.add(a+"->"+nums[i]);
+            }else{
+                res.add(a+"");
+            }
+        }
+        return res;
+    }
+}
