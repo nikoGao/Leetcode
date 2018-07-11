39f1cda7f5a6e7bc16244b8699d817bdb955b04e
Niko Gao <niko_fan@sina.com>
2018-07-11 17:25:19 -0400

    Misread the question, correct now

--- 531. Lonely Pixel I/AC_Answer.java
+++ 531. Lonely Pixel I/AC_Answer.java
@@ -2,23 +2,23 @@
 class Solution{
 	public int findLonelyPixel(char[][] picture){
 		int result = 0;
+		int[] rows = new int[picture.length];
+		if(rows==0) return result;
+		int[] cols = new int[picture[0].length];
 		for(int i=0; i<picture.length; i++){
 			for(int j=0; j<picture[i].length; j++){
-				if(picture[i][j] == 'B' && check(picture, i, j)){
-					result++;
+				if(picture[i][j] == 'B'){
+					++rows[i];
+					++cols[j];
 				}
+			}
+		}
+		for(int i=0; i<picture.length; i++){
+			for(int j=0; j<picture[i].length; j++){
+				if(picture[i][j] == 'B' && rows[i]==1 && cols[j]==1) ++result;
 			}
 		}
 		return result;
 	}
 
-	public boolean check(char[][] picture, int row, int col){
-		// check if a block is lonely
-		boolean lonely = true;
-		if(row>0) lonely &= (picture[row-1][col]!='B');
-		if(row<picture[row].length-1) lonely &= (picture[row+1][col]!='B');
-		if(col>0) lonely &= (picture[row][col-1]!='B');
-		if(col<picture.length-1) lonely &= (picture[row][col+1]!='B');
-		return lonely;
-	}
 }
