5be9d7280860d59fe53804836ada2410e750891f
Niko Gao <niko_fan@sina.com>
2018-08-05 22:20:00 -0400

    The new one is faster than older one

---
+++ 690.Employee Importance/AC_Answer_impro.java
@@ -0,0 +1,29 @@
+/*
+// Employee info
+class Employee {
+    // It's the unique id of each node;
+    // unique id of this employee
+    public int id;
+    // the importance value of this employee
+    public int importance;
+    // the id of direct subordinates
+    public List<Integer> subordinates;
+};
+*/
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        Map<Integer, Employee> employers = new HashMap<>();
+        for(Employee e:employees) employers.put(e.id, e);
+        Queue<Employee> q = new LinkedList<>();
+        q.offer(employers.get(id));
+        int result = 0;
+        while(q.size()>0){
+            Employee e = q.poll();
+            result += e.importance;
+            for(int i:e.subordinates){
+                q.offer(employers.get(i));
+            }
+        }
+        return result;
+    }
+}
